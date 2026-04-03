6b158838b701d30e7bb2c9391dceac945a432e34
WolfY <david.skliarov.dev@gmail.com>
2026-04-03 12:12:06 +0400

    refactor: remove border for the button

--- src/components/ui/SolidBtn.tsx
+++ src/components/ui/SolidBtn.tsx
@@ -8,7 +8,7 @@ export default function SolidBtn({
 	return (
 		<button
 			type="button"
-			className={`bg-[#4F46E5] px-4 py-3 border-2 rounded-lg font-medium text-white capitalize ${styles}`}
+			className={`bg-[#4F46E5] px-4 py-3 rounded-lg font-medium text-white capitalize ${styles}`}
 		>
 			{children}
 		</button>
