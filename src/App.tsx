ead4b129e507357de69ec8c3d91593f99a51ac66
WolfY <david.skliarov.dev@gmail.com>
2026-04-03 14:35:02 +0400

    fix: Route element syntax, add dashboard element

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
+import Dashboard from "./components/layout/Dashboard";
 import Navbar from "./components/Navbar";
 import "@fontsource-variable/inter/wght.css";
 
 function App() {
 	return (
-		<div className="bg-gray-100">
+		<div className="bg-gray-100 w-screen">
 			<Navbar />
 			<Routes>
-				<Route path="/"></Route>
-				<Route path="/login"></Route>
-				<Route path="/login"></Route>
+				<Route path="/" element={<Dashboard />} />
+				{/* <Route path="/login" element={} /> */}
+				{/* <Route path="/login" element={} /> */}
 			</Routes>
 			<Footer />
 		</div>
